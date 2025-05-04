413f15dd7fbd191cf6c90608caf3471338b4a715
Batsatsugo <corajianchristian@gmail.com>
2025-05-04 13:48:40 +0800

    added a comment for function

--- the_ultimate_quiz_game.py
+++ the_ultimate_quiz_game.py
@@ -1,5 +1,6 @@
 # import JSON module to work with the ultimate quiz data saved in JSON format
 import json
 
+# function to load quiz data and run the quiz
 def run_quiz():
     filename = "the_ultimate_quiz_data.txt"
